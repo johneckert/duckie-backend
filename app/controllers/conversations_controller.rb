c1cd4ae9366565dd400f713ae820a0a9a361f39c
johneckert <johnteckert@gmail.com>
2018-03-16 11:25:06 -0400

    keywords being created from watson response

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -16,19 +16,27 @@ class ConversationsController < ApplicationController
     @conversation = Conversation.create(user_id: conversation_params[:user_id], transcript: conversation_params[:transcript])
     #organize for watson
     parameters = {
-        'text' => @conversation.transcript,
-        'features' => {
-            'keywords' =>
-            {
-              'emotion' => false,
-              'sentiment' => false
-            }
-          },
-      'return_analyzed_text' => true
+      'text' => @conversation.transcript,
+      'features' => {
+        'concepts' => {
+          'limit' => 10
+        },
+        'keywords' =>
+        {
+          'emotion' => false,
+          'sentiment' => false,
+          'limit' => 10
+        }
+      }
     }.to_json
+
+    #send transcript to watson and get keyword respons
     response = RestClient::Request.execute method: :post, url: "https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2017-02-27", user: ENV["watson_username"], password: ENV["watson_password"], headers: {'Content-Type': "application/json"}, payload: parameters
 
-    puts response
+    #generate keyword objects
+    converted_response = JSON.parse(response)
+    create_keywords_from_keywords(converted_response)
+    create_keywords_from_concepts(converted_response)
 
     render json: @conversation, status: 201
   end
@@ -43,4 +51,16 @@ class ConversationsController < ApplicationController
     params.permit(:user_id, :transcript)
   end
 
+  #helpers for creating keyword instances
+  def create_keywords_from_keywords(response_hash)
+    response_hash['keywords'].map do |keyword_obj|
+      Keyword.create_with(relevance: keyword_obj['relevance']).find_or_create_by(word: "#{keyword_obj['text']}")
+    end
+  end
+
+  def create_keywords_from_concepts(response_hash)
+    response_hash['concepts'].map do |concept_obj|
+      Keyword.create_with(relevance: concept_obj['relevance']).find_or_create_by(word: "#{concept_obj['text']}")
+    end
+  end
 end
--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,2 +1,18 @@
 class KeywordsController < ApplicationController
+
+  def index
+    keywords = Keyword.all
+    render json: keywords, status: 200
+  end
+
+  private
+
+  def find_keyword
+    @keyword = Keyword.find_by(id: conversation_params[:id])
+  end
+
+  def keyword_params
+    params.permit(:word, :relevance)
+  end
+
 end
